ed4ba250386bc25310af354d102e8e758c0952d4
GitHub Action <clsrfish@gmail.com>
2026-09-09 00:37:32 +0800

    Deploy knowledgebase 2026-09-09 00:37:32

---
+++ postscript-1d7bbe70.js
@@ -0,0 +1,12 @@
+await Promise.all([
+  import("./static/scripts/script-0-f7c52b6a.js"),
+  import("./static/scripts/script-1-f1365b3a.js"),
+  import("./static/scripts/script-2-1f9394e6.js"),
+  import("./static/scripts/script-3-af2773e5.js"),
+  import("./static/scripts/script-4-cc8afa9d.js"),
+  import("./static/scripts/script-5-7e107d7e.js"),
+  import("./static/scripts/script-6-5ef560a8.js"),
+  import("./static/scripts/script-7-c9e900a7.js"),
+  import("./static/scripts/script-8-be15df42.js")
+]);
+await import("./static/scripts/script-9-d93af9ba.js");
